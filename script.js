8b4937b5f0bd341c952fd7054f33e5183af456d0
oamaynar <oamaynar@ncsu.edu>
2025-04-25 18:26:16 -0400

    Pushing starter files again

---
+++ script.js
@@ -0,0 +1,9 @@
+function processEquation() {
+    // Get user input
+    const equation = document.getElementById('equationInput').ariaValueMax;
+
+    // Dummy output for now
+    document.getElementById('outputArea').innerText = "You entered: " + equation;
+
+    // Here is where parsing and uncertainty calculations will actually happen
+}
